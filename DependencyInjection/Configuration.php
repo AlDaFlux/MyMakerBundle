a7d29b5bdcbe4ecb26e97f9773690b0fc2aae519
Ubuntu <ubuntu@vps-95f47936.vps.ovh.net.novalocal>
2021-07-30 14:59:12 +0000

    2021

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Aldaflux\MyMakerBundle\DependencyInjection;
+use Symfony\Component\HttpKernel\Kernel;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -17,8 +18,24 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('aldaflux_my_maker');
-        $rootNode = $treeBuilder->getRootNode();
+//        $treeBuilder = new TreeBuilder('aldaflux_my_maker');
+        $treeBuilder = new TreeBuilder('aldaflux_mymaker');
+
+        if (Kernel::VERSION_ID >= 40200) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $rootNode = $treeBuilder->root('aldaflux_mymaker');
+        }        
+        
+        $rootNode->children()
+        ->arrayNode('admin')
+            ->children()
+                ->scalarNode('folder')->end()
+                ->scalarNode('folder_admin')->end()
+            ->end()
+        ->end()
+    ->end()
+;
         return $treeBuilder;
     }
 
